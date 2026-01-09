f87dffcbd0fcde9d7f40c07ef04346925f5f8b49
Karan Shergill <188668929+karan-shergill@users.noreply.github.com>
2026-01-09 17:54:05 +0530

    Create e_guess_number_higher_or_lower.java

---
+++ practice/binary_search/e_guess_number_higher_or_lower.java
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/guess-number-higher-or-lower/description/
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return       -1 if num is higher than the picked number
+ *                1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int start = 1;
+        int end = n;
+
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+
+            int value = guess(mid);
+
+            if (value == 0) {
+                return mid;
+            } else if (value == -1) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
+
+/* Learning:
+1. Problem discription needs to be read properly before starting
+
+TC: nlogn
+SC: O(1)
+*/
