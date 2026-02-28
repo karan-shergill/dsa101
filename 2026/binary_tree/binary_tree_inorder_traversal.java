fbad7ec3b70fd79f64c4e73fcd59f50e6b832faa
Karan Shergill <188668929+karan-shergill@users.noreply.github.com>
2026-02-28 15:11:19 +0530

    Create binary_tree_inorder_traversal.java

---
+++ 2026/binary_tree/binary_tree_inorder_traversal.java
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+
+// Recursive
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new LinkedList<>();
+        inorderTraversal(root, list);
+        return list;
+    }
+
+    public void inorderTraversal(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+
+        inorderTraversal(root.left, list);
+        list.add(root.val);
+        inorderTraversal(root.right, list);
+    }
+}
+
+// Iterative
+class Tree {
+    // Return a list containing the inorder traversal of the given tree
+    ArrayList<Integer> inOrder(Node root) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        if (root == null)
+            return arrayList;
+            
+        Stack<Node> stack = new Stack<>();
+        Node currNode = root;
+        
+        while(stack.size() > 0 || currNode != null) {
+            while (currNode != null) {
+                stack.push(currNode);
+                currNode = currNode.left;
+            }
+            if(stack.size() > 0) {
+                currNode = stack.pop();
+                arrayList.add(currNode.data);
+                currNode = currNode.right;
+            }
+        }
+        
+        return arrayList;
+    }
+}
