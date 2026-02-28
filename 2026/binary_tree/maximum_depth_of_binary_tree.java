9ffa872e42c3db2f7bfef27d2f96934ec8e95baf
Karan Shergill <188668929+karan-shergill@users.noreply.github.com>
2026-02-28 14:51:42 +0530

    Create maximum_depth_of_binary_tree.java

---
+++ 2026/binary_tree/maximum_depth_of_binary_tree.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+
+// Recursive
+class Solution {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = 1 + maxDepth(root.left);
+        int right = 1 + maxDepth(root.right);
+
+        return Math.max(left, right);
+    }
+}
