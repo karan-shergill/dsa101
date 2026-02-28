929de913ee3d2b2dbf3c3ecae8bb9e66041bc55e
Karan Shergill <188668929+karan-shergill@users.noreply.github.com>
2026-02-28 14:50:02 +0530

    Create balanced_binary_tree.java

---
+++ 2026/binary_tree/balanced_binary_tree.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/balanced-binary-tree/description/
+
+// Recursive
+class Solution {
+
+    public boolean isBalanced(TreeNode root) {
+        boolean[] ans = new boolean[1];
+        ans[0] = true;
+
+        isBalanced(root, ans);
+        return ans[0];
+    }
+
+    public int isBalanced(TreeNode root, boolean[] ans) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = 1 + isBalanced(root.left, ans);
+        int right = 1 + isBalanced(root.right, ans);
+
+        if (ans[0]) {
+            ans[0] = Math.abs(left - right) <= 1;
+        }
+
+        return Math.max(left, right);
+    }
+}
